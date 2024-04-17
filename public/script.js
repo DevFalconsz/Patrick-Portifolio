339c7c0fd8279dfffc4b49d7fbe854e6493e99c3
Patrick Oliveira Vagetti <77699722+DevFalconsz@users.noreply.github.com>
2024-04-17 20:54:45 -0300

    Add files via upload

---
+++ public/script.js
@@ -0,0 +1,15 @@
+document.getElementById("card1").addEventListener("click", function() {
+    window.location.href = "https://www.instagram.com/pk_vagetti_/";
+});
+
+document.getElementById("card2").addEventListener("click", function() {
+    window.location.href = "https://api.whatsapp.com/send?phone=62982563379&text=Olá,%20quero%20fazer%20um%20orçamento";
+});
+
+document.getElementById("card3").addEventListener("click", function() {
+    window.location.href = "https://github.com/DevFalconsz";
+});
+
+document.getElementById("card4").addEventListener("click", function() {
+    window.location.href = "https://discord.com/users/459121743131443210";
+});
